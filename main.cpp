89162c7547be24ac5c9bb23dead7c8618a25d2a0
Soham Kudalkar <sohamskudalkar02@gmail.com>
2025-04-01 00:46:02 +0530

    add angle and magnitude setting

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <iostream>
 #include <raylib.h>
 
 struct Ball {
@@ -7,18 +9,42 @@ struct Ball {
 	Color color;
 };
 
+double toRadian(double degree) { return degree / 180 * PI; }
+
 int main() {
 	const int screenWidth = 1280;
 	const int screenHeight = 720;
 
+	float angle;
+	float magnitude;
+
+	std::cout << "Enter angle of the ball: ";
+	std::cin >> angle;
+	std::cout << "Enter the speed of the ball: ";
+	std::cin >> magnitude;
+
 	Ball ball;
 	ball.location = Vector2{screenWidth / 2.0f, screenHeight / 2.0f};
+	ball.velocity = Vector2{float(magnitude * cos(toRadian(angle))),
+				float(magnitude * sin(toRadian(angle)))};
 	ball.color = RED;
 	ball.radius = 40;
 
 	InitWindow(screenWidth, screenHeight, "Bouncing ball");
 
 	while (!WindowShouldClose()) {
+		float delta = GetFrameTime();
+		ball.location.x += ball.velocity.x * delta;
+		ball.location.y += ball.velocity.y * delta;
+
+		if (ball.location.x + ball.radius >= screenWidth ||
+		    ball.location.x - ball.radius <= 0) {
+			ball.velocity.x = -ball.velocity.x;
+		}
+		if (ball.location.y + ball.radius >= screenHeight ||
+		    ball.location.y - ball.radius <= 0) {
+			ball.velocity.y = -ball.velocity.y;
+		}
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		DrawCircleV(ball.location, ball.radius, ball.color);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Bouncing ball
+## References:
+> https://www.raylib.com/examples.html
+> https://math.stackexchange.com/questions/1636838/writing-unit-vector-r-in-terms-of-sine-and-cosine
