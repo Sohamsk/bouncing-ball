329767f2acae04be9f381627dca54a4dd7d67dc5
Soham Kudalkar <sohamskudalkar02@gmail.com>
2025-05-01 18:37:24 +0530

    added gravity and clamped ball

--- src/updateBall.cpp
+++ src/updateBall.cpp
@@ -1,6 +1,8 @@
 #include <ball.hpp>
 #include <cmath>
 
+const double g = 981;
+
 double toRadian(double degree) { return degree / 180 * PI; }
 
 void Ball::initBall(float radius, float angle, float magnitude, Color color,
@@ -13,15 +15,25 @@ void Ball::initBall(float radius, float angle, float magnitude, Color color,
 }
 
 void Ball::updateBall(float delta, int screenWidth, int screenHeight) {
+	// handle gravity
+	this->velocity.y += g * delta;
+
 	this->location.x += this->velocity.x * delta;
 	this->location.y += this->velocity.y * delta;
 
-	if (this->location.x + this->radius >= screenWidth ||
-	    this->location.x - this->radius <= 0) {
+	// clamp the ball
+	if (this->location.x + this->radius >= screenWidth) {
 		this->velocity.x = -this->velocity.x;
+		this->location.x = screenWidth - this->radius;
+	} else if (this->location.x - this->radius <= 0) {
+		this->velocity.x = -this->velocity.x;
+		this->location.x = 0 + this->radius;
 	}
-	if (this->location.y + this->radius >= screenHeight ||
-	    this->location.y - this->radius <= 0) {
+	if (this->location.y + this->radius >= screenHeight) {
 		this->velocity.y = -this->velocity.y;
+		this->location.y = screenHeight - this->radius;
+	} else if (this->location.y - this->radius <= 0) {
+		this->velocity.y = -this->velocity.y;
+		this->location.y = this->radius;
 	}
 }
